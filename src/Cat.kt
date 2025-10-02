e7d22de424c34360fb404547318aef5824b768b8
Fimy32 <benjaminhaigh05@gmail.com>
2025-10-02 14:35:46 +0100

    Modifed eat and walk functions to be more robust

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,8 +1,7 @@
 class Cat (val name: String, var age: Int, var weight: Int) {
 
-    public fun walk(distance: Int) : Boolean
-    {
-        if(weight - distance >= 5) {
+    public fun walk(distance: Int): Boolean {
+        if (weight - distance >= 5) {
             weight -= distance
             return true
         }
@@ -11,13 +10,16 @@ class Cat (val name: String, var age: Int, var weight: Int) {
 
     public fun eat(amount: Int) {
         for (i in 0..amount) {
-            while (weight < 20) {
+            if (weight < 20) {
                 weight += 1
+            } else {
+                return False
             }
+            return True
         }
     }
 
-    override fun toString() : String {
+    override fun toString(): String {
         return "Name: $name Age: $age"
     }
 }
